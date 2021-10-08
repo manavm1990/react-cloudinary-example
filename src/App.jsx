53a7012b07573fa33ad1c73060a38ee4dba39e3f
Manav Misra <manavm@visionwebsoft.com>
2021-10-07 21:27:14 -0500

    :construction: Add image only upload button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <main></main>;
+  const handleChange = () => {
+    console.log("change");
+  };
+  return (
+    <main className="h-screen w-full flex justify-center items-center">
+      <input type="file" onChange={handleChange} accept="image/*" />
+    </main>
+  );
 }
 
 export default App;
